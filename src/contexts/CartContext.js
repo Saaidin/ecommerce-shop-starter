d4d03bd0364a7081a7f955147c9963b170e1773e
saaidin <dinme73@gmail.com>
2023-04-20 01:55:57 +0800

    1:35:15-Cart counter is functioning at Header sec

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,9 +1,21 @@
-import React, { createContext, useState, useEffect } from "react"
+import React, { createContext, useEffect, useState } from "react"
 
 export const CartContext = createContext()
 
 const CartProvider = ({ children }) => {
+  //cart state
   const [cart, setCart] = useState([])
+  //item amount state
+  const [itemAmount, setItemAmount] = useState(0)
+  //update item amount
+  useEffect(() => {
+    if (cart) {
+      const amount = cart.reduce((accumulator, currentItem) => {
+        return accumulator + currentItem.amount
+      }, 0)
+      setItemAmount(amount)
+    }
+  }, [cart])
 
   const addToCart = (product, id) => {
     const newItem = { ...product, amount: 1 }
@@ -33,8 +45,47 @@ const CartProvider = ({ children }) => {
     setCart(newCart)
   }
 
+  const clearCart = () => {
+    setCart([])
+  }
+
+  const increaseAmount = (id) => {
+    const cartItem = cart.find((item) => item.id === id)
+    addToCart(cartItem, id)
+  }
+
+  const decreaseAmount = (id) => {
+    const cartItem = cart.find((item) => {
+      return item.id === id
+    })
+    if (cartItem) {
+      const newCart = cart.map((item) => {
+        if (item.id === id) {
+          return { ...item, amount: cartItem.amount - 1 }
+        } else {
+          return item
+        }
+      })
+      setCart(newCart)
+    }
+
+    if (cartItem.amount < 2) {
+      removeFromCart(id)
+    }
+  }
+
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+    <CartContext.Provider
+      value={{
+        cart,
+        addToCart,
+        removeFromCart,
+        clearCart,
+        increaseAmount,
+        decreaseAmount,
+        itemAmount,
+      }}
+    >
       {children}
     </CartContext.Provider>
   )
