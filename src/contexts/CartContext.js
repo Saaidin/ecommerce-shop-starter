f6a05dd023d2c861f7d8bc4665b1017f45f43628
saaidin <dinme73@gmail.com>
2023-04-20 05:19:04 +0800

    1:40:15-total amount functioning on Sidebar

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -7,6 +7,16 @@ const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([])
   //item amount state
   const [itemAmount, setItemAmount] = useState(0)
+  //total price state
+  const [total, setTotal] = useState(0)
+
+  useEffect(() => {
+    const total = cart.reduce((accumulator, currentItem) => {
+      return accumulator + currentItem.price * currentItem.amount
+    }, 0)
+    setTotal(total)
+  })
+
   //update item amount
   useEffect(() => {
     if (cart) {
@@ -84,6 +94,7 @@ const CartProvider = ({ children }) => {
         increaseAmount,
         decreaseAmount,
         itemAmount,
+        total,
       }}
     >
       {children}
