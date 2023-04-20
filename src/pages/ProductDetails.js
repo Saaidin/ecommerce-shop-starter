ac9cf7df8a2521dd0a52b19d968fd2b3466ce393
saaidin <dinme73@gmail.com>
2023-04-21 02:46:01 +0800

    1:50:00-Finish Footer section

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useContext } from "react"
+import { useParams } from "react-router-dom"
+import { CartContext } from "../contexts/CartContext"
+import { ProductContext } from "../contexts/ProductContext"
 
 const ProductDetails = () => {
-  return <div>Product Details Page</div>;
-};
+  const { id } = useParams()
+  // console.log(id)
+  const { products } = useContext(ProductContext)
+  const { addToCart } = useContext(CartContext)
+  return <div>Product Details Page</div>
+}
 
-export default ProductDetails;
+export default ProductDetails
